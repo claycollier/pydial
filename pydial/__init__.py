5a55c10d5100e2a7e485535b9ebdc4d16d64d755
Clay Collier <clay.collier@gmail.com>
2014-04-05 18:18:45 -0400

    Export server from top level module

--- pydial/__init__.py
+++ pydial/__init__.py
@@ -1,6 +1,7 @@
 from .client import discover 
 from .client import DialClient
+from /server import DialServer
 
-__all__ = ['discover', 'DialClient']
+__all__ = ['discover', 'DialClient', 'DialServer']
 
 
